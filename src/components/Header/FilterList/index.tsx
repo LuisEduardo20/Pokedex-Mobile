83346f74d503d2d2b9ff8b7d6009b6b05038aceb
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-03-03 15:41:44 -0300

    Função de remoção do nome da lista de filtros

---
+++ src/components/Header/FilterList/index.tsx
@@ -0,0 +1,37 @@
+import React, {useContext} from 'react';
+import {IconButton} from 'react-native-paper';
+import {HeaderContext} from '../../../contexts/HeaderContext';
+import {SortContainer, SortButton, SortButtonText} from './styles';
+
+const FilterList = () => {
+  const {filterList, setFilterList} = useContext(HeaderContext);
+
+  const removeFilter = (tagName: string) => {
+    setFilterList(() =>
+      filterList.filter((name: string) => {
+        return name !== tagName;
+      }),
+    );
+  };
+
+  return (
+    <SortContainer horizontal={true}>
+      {filterList.map((tagName: string, index: number) => {
+        return (
+          <SortButton key={index}>
+            <SortButtonText>{tagName}</SortButtonText>
+            <IconButton
+              icon="close-circle"
+              size={17}
+              color={'#5E5D5D'}
+              style={{padding: 0, marginLeft: 5}}
+              onPress={() => removeFilter(tagName)}
+            />
+          </SortButton>
+        );
+      })}
+    </SortContainer>
+  );
+};
+
+export default FilterList;
