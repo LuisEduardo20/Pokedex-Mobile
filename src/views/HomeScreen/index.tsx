6473d65c01dc19e184aebf2c898b7b2919821fee
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-03-02 22:31:23 -0300

    Adição do Header na tela Home

--- src/views/HomeScreen/index.tsx
+++ src/views/HomeScreen/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Container, Title} from './styles';
+import Header from '../../components/Header';
+import {Container} from './styles';
 
 const Home = () => {
   return (
     <Container>
-      <Title>Home</Title>
+      <Header />
     </Container>
   );
 };
--- src/views/HomeScreen/styles.ts
+++ src/views/HomeScreen/styles.ts
@@ -2,11 +2,4 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const Title = styled.Text`
-  font-size: 24px;
-  color: red;
 `;
