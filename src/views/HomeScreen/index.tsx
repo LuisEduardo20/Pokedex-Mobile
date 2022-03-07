2aaabe9575ad7f7efc91bab8fb3be0a0a18cf3ec
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-03-07 09:43:26 -0300

    Adição do componente de lista dos pokemons

--- src/views/HomeScreen/index.tsx
+++ src/views/HomeScreen/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header';
+import PokemonList from '../../components/PokemonList';
 import {Container} from './styles';
 
 const Home = () => {
   return (
     <Container>
       <Header />
+      <PokemonList />
     </Container>
   );
 };
