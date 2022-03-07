cef8c859185b8942ed4711a65c7fdcf620f144f5
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-03-07 09:42:59 -0300

    Adição do Provider do react-redux

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {Provider} from 'react-redux';
+import store from './src/store';
+
 import LoginScreen from './src/views/LoginScreen';
 import HomeScreen from './src/views/HomeScreen';
 
@@ -10,21 +15,23 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <SafeAreaView style={styles.view}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          {/* <Stack.Screen
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            {/* <Stack.Screen
             name="Login"
             component={LoginScreen}
             options={{headerShown: false}}
           /> */}
 
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{headerShown: false}}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
     </SafeAreaView>
   );
 };
